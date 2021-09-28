c316b859192fdb660c55b7666cdb069cc4ac3b9e
sbhsxsb <botaosu296@hotmail.com>
2021-09-27 20:46:03 -0400

    I am just practicing

---
+++ src/main/java/Practice.java
@@ -0,0 +1,21 @@
+public class Practice implements Tradable, Drivable{
+    @Override
+    public void upgradeSpeed() {
+
+    }
+
+    @Override
+    public void downgradeSpeed() {
+
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return 0;
+    }
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+}
